2d8701c80cc0505d202bbc3523cdebba4384c7a1
João Lousada <joao.lousada1@gmail.com>
2019-12-09 00:21:51 +0000

    Add files via upload
    adding runtesthistory csv file to be filled as tests are run. 
    PATCH: generation of commit list and test list less randomized, beginning to emulate intended patterns. (not completed)

--- dummy_data/dataGenerator.py
+++ dummy_data/dataGenerator.py
@@ -1,12 +1,16 @@
 import csv
 import random
+from random import randrange
+import datetime
+
+import pandas as pd
 
 records = 10
 print("Making %d records\n" % records)
 
 fieldnames_src = ['id', 'prevalence', 'stability']
-fieldnames_test = ['id', 'time', 'dependence']
-fieldnames_commits = ['id', 'who', 'what_files', 'broken_files']
+fieldnames_test = ['id', 'time_to_run', 'dependence']
+fieldnames_commits = ['commit_id', 'author', 'timestamp', 'modified_files', 'broken_files']
 
 writer_src = csv.DictWriter(open("src_files.csv", "w"), fieldnames=fieldnames_src)
 writer_test = csv.DictWriter(open("test_files.csv", "w"), fieldnames=fieldnames_test)
@@ -15,35 +19,50 @@ writer_commits = csv.DictWriter(open("cmt_files.csv", "w"), fieldnames=fieldname
 sources = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
 people = ['Donnatelo', 'Rafael', 'Leonardo', 'Michelangelo']
 
-# filling src_files.csv
-writer_src.writerow(dict(zip(fieldnames_src, fieldnames_src)))
-for i in range(0, records):
-    writer_src.writerow(dict([
-        ('id', sources[i]),
-        ('prevalence', str(random.randint(0, 100))),
-        ('stability', str(random.randint(0, 100)))]))
 
-# filling test_files.csv
+# timestamp generator
+def random_date(start, l):
+    current = start
+    while l >= 0:
+        curr = current + datetime.timedelta(minutes=randrange(60))
+        yield curr
+        l -= 1
+
+
+startDate = datetime.datetime(2013, 9, 20, 13, 00)
+timestamps = []
+for x in random_date(startDate, 10):
+    timestamps.append(x.strftime("%d/%m/%y %H:%M"))
+timestamps = sorted(timestamps)
+
+writer_src.writerow(dict(zip(fieldnames_src, fieldnames_src)))
 writer_test.writerow(dict(zip(fieldnames_test, fieldnames_test)))
+writer_commits.writerow(dict(zip(fieldnames_commits, fieldnames_commits)))
+
 for i in range(0, records):
+    # Generates Test files list
+    deps = random.sample(sources, k=random.randint(1, 4))
     writer_test.writerow(dict([
         ('id', i),
-        ('time', str(random.randint(5, 60))),
-        ('dependence', random.sample(sources, k=random.randint(1, 4)))]))
+        ('time_to_run', str(random.randint(5, 60))),
+        ('dependence', deps)]))
 
-# filling cmt_files.csv
-writer_commits.writerow(dict(zip(fieldnames_commits, fieldnames_commits)))
-for i in range(0, records):
+    # Generates Commit List
     cmt1 = random.sample(sources, k=random.randint(1, 4))
     writer_commits.writerow(dict([
-        ('id', i),
-        ('who', random.choice(people)),
-        ('what_files', cmt1),
+        ('commit_id', 213 * i % 100 + 3 * i),
+        ('author', random.choice(people)),
+        ('timestamp', timestamps[i]),
+        ('modified_files', cmt1),
         ('broken_files', random.sample(cmt1, k=random.randint(0, len(cmt1))))]))
 
+tests = pd.read_csv('test_files.csv')
+print(tests)
+prevalence = writer_test['dependence'].count('a')
 
-
-
-
-
-
+for i in range(0, records):
+    # Generates Source File list
+    writer_src.writerow(dict([
+        ('id', sources[i]),
+        ('prevalence', str(prevalence)),
+        ('stability', str(random.randint(0, 100)))]))
--- dummy_data/runTests.py
+++ dummy_data/runTests.py
@@ -1,3 +1,6 @@
+import csv
+import random
+
 import pandas as pd
 import numpy as np
 
@@ -24,4 +27,25 @@ print('COMMIT LIST')
 print("Data shape is: ", src.shape, "\t.....\t(instances, variables)")
 print(commit)
 print("------------------------------------------------------------------------")
+
+print("------------------------------------------------------------------------")
+# run test history contains the history of all tests that have been run.
+print('RUN TEST HISTORY')
+records = src.size()
+
+fieldnames_runtesthistory = ['commit_id', 'test_id', 'timestamp', 'test_status', 'this_commit_estimated_status']
+writer_runtesthistory = csv.DictWriter(open("rth_files.csv", "w"), fieldnames=fieldnames_runtesthistory)
+writer_runtesthistory.writerow(dict(zip(fieldnames_runtesthistory, fieldnames_runtesthistory)))
+
+for i in range(0, records):
+    # Generates Source File list
+    writer_runtesthistory.writerow(dict([
+        ('commit_id', ),
+        ('test_id', ),
+        ('timestamp',),
+        ('test_status',),
+        ('this_commit_estimated_status', str(random.randint(0, 100)))]))
+
+print("------------------------------------------------------------------------")
+
 
